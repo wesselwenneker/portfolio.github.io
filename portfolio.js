f522d94056b84333dafdeef46f882ef80d7c0431
Wessel Wenneker <wessel.wenneker@coderclass.nl>
2024-03-28 12:28:51 +0100

    bijna klaar

---
+++ portfolio.js
@@ -0,0 +1,34 @@
+function openKas() {
+    document.getElementById("slimmekas_overlay").style.height = "100%";
+  }
+  
+function closeKas() {
+    document.getElementById("slimmekas_overlay").style.height = "0%";
+}
+
+
+function openVoetbalBot() {
+    document.getElementById("voetbalBot_overlay").style.height = "100%";
+}
+  
+function closeVoetbalBot() {
+    document.getElementById("voetbalBot_overlay").style.height = "0%";
+}
+
+
+function openSolenoid() {
+    document.getElementById("solenoid_overlay").style.height = "100%";
+}
+  
+function closeSolenoid() {
+    document.getElementById("solenoid_overlay").style.height = "0%";
+}
+
+
+function openCpp() {
+    document.getElementById("cpp_overlay").style.height = "100%";
+}
+  
+function closeCpp() {
+    document.getElementById("cpp_overlay").style.height = "0%";
+}
